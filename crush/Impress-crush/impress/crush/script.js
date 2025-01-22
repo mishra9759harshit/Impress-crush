5716a29d6955c591fde6fc62850cd0e29e08c7e8
mishra9759harshit <mishra9759harshit@gmail.com>
2025-01-22 19:48:59 +0530

    I fixed issue with custom cursor
    I added Heart emojii as cursor

--- crush/Impress-crush/impress/crush/script.js
+++ crush/Impress-crush/impress/crush/script.js
@@ -172,7 +172,7 @@ class Paper {
     this.holdingPaper = false;
     this.rotating = false;
   
-    // Reset cursor to heart emoji when mouse up
+    // Reset cursor to heart emoji when mouse up stlye In C
     const paper = document.querySelector('.paper');
     paper.style.cursor = 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'48\' height=\'48\' viewBox=\'0 0 48 48\'%3E%3Ccircle cx=\'24\' cy=\'24\' r=\'22\' fill=\'none\' stroke=\'#ff0000\' stroke-width=\'2\' /%3E%3Ctext x=\'10\' y=\'30\' font-size=\'24\' fill=\'#ff0000\'%3E❤️%3C/text%3E%3C/svg%3E") 24 24, auto';
   }
